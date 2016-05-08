83ff1b70048f210545af93586d099ebc71e6030d
arturomtm <arturomtm@gmail.com>
2016-05-08 02:02:44 +0200

    remote event listening feature addedgit status

--- test/background/index.js
+++ test/background/index.js
@@ -1,11 +1,23 @@
+var EventEmitter = require('eventemitter3');
+
 var rmi = require("../../lib/server");
 
-console.log(rmi);
+console.log("[TestExtension:Background]", rmi);
 
-var test = {
+function Test(){
+  this.i = 0;
+};
+Test.prototype = Object.create(EventEmitter.prototype);
+
+var test = new Test();
+Object.assign(test, {
   echo: function(msg) {
     return msg;
   }
-}
+});
+
+setInterval(function(){
+  test.emit("testevent", ++test.i);
+}, 1000);
 
 rmi.register("test", test);
--- test/content/index.js
+++ test/content/index.js
@@ -3,5 +3,8 @@ var rmi = require("../../lib/client")();
 console.log(rmi);
 
 rmi.getInstance("test")
-.then(test => test.echo("hello"))
+.then(test => {
+  test.on("testevent", i => console.log("[TestExtension:Content]", i));
+  return test.echo("hello")
+})
 .then(msg => console.log(msg))
